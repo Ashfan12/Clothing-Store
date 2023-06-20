8ce90f1e1060b8238e27b1996089ee0a62159934
Ashfan12 <116094511+Ashfan12@users.noreply.github.com>
2023-06-20 06:01:50 +0530

    Add files via upload

---
+++ Source/data.js
@@ -0,0 +1,114 @@
+let shopItemsData = [
+  {
+    id: 'Prod1',
+    name: 'Casual Kazak',
+    price: 45,
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing',
+    img: 'Images/Image1.jpg'
+  },
+  {
+    id: 'Prod2',
+    name: 'Office Shirt',
+    price: 100,
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing',
+    img: 'Images/Image2.jpg'
+  },
+  {
+    id: 'Prod3',
+    name: 'T-shirt',
+    price: 25,
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing',
+    img: 'Images/Image3.jpg'
+  },
+  {
+    id: 'Prod4',
+    name: 'Mens Suit',
+    price: 245,
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing',
+    img: 'Images/Image4.jpg'
+  },
+  {
+    id: 'Prod5',
+    name: 'Leather Shoe',
+    price: 85,
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing',
+    img: 'Images/Image5.jpeg'
+  },
+  {
+    id: 'Prod6',
+    name: 'Mens Shorts',
+    price: 176,
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing',
+    img: 'Images/Image6.jpg'
+  },
+  {
+    id: 'Prod7',
+    name: 'Football Jersey',
+    price: 37,
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing',
+    img: 'Images/Image7.jpg'
+  },
+  {
+    id: 'Prod8',
+    name: 'Converse Shoes',
+    price: 142,
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing',
+    img: 'Images/Image8.jpg'
+  },
+  {
+    id: 'Prod9',
+    name: 'Caps for Men',
+    price: 27,
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing',
+    img: 'Images/Image9.jpg'
+  },
+  {
+    id: 'Prod10',
+    name: 'Indian Shawl',
+    price: 9,
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing',
+    img: 'Images/Image10.jpg'
+  },
+  {
+    id: 'Prod11',
+    name: 'Anarkali Salwar',
+    price: 50,
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing',
+    img: 'Images/Image11.jpg'
+  },
+  {
+    id: 'Prod12',
+    name: 'Long Frock',
+    price: 49,
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing',
+    img: 'Images/Image12.jpg'
+  },
+  {
+    id: 'Prod13',
+    name: 'Anarkali Salwar',
+    price: 55,
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing',
+    img: 'Images/Image13.jpg'
+  },
+  {
+    id: 'Prod14',
+    name: 'Womens Jeans',
+    price: 127,
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing',
+    img: 'Images/Image14.jpeg'
+  },
+  {
+    id: 'Prod15',
+    name: 'Mens Jeans',
+    price: 175,
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing',
+    img: 'Images/Image15.jpg'
+  },
+  {
+    id: 'Prod16',
+    name: 'Winter Jacket',
+    price: 300,
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing',
+    img: 'Images/Image16.jpeg'
+  },
+]
